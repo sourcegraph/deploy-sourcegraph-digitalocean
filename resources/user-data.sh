77289cc9c9126f3515465efdd114e96068b9d46d
Keegan Carruthers-Smith <keegan@sourcegraph.com>
2019-05-08 15:43:23 +0200

    Update to 3.3.8

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.3.7
+export SOURCEGRAPH_VERSION=3.3.8
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
