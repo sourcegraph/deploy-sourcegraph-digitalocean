c0fdc62b62145db535a33c65a44850c4eb5e1586
Stephen Gutekanst <stephen.gutekanst@gmail.com>
2019-06-13 13:06:50 -0700

    Release Sourcegraph v3.4.4

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.4.3
+export SOURCEGRAPH_VERSION=3.4.4
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
