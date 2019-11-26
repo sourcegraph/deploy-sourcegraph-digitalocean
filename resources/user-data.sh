be0d7bd31ea8f8bdecf9e74e3813abda25bdcf2b
Beyang Liu <beyang@sourcegraph.com>
2019-11-25 22:01:50 -0800

    Update latest release to 3.10.1 (#25)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.10.0
+export SOURCEGRAPH_VERSION=3.10.1
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
