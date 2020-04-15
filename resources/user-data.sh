ea11e3b4f7b7e7bb9e9713adf1f523f9cc07a080
uwedeportivo <534011+uwedeportivo@users.noreply.github.com>
2020-04-14 21:45:42 -0700

    Update latest release to 3.14.3 (#49)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.14.2
+export SOURCEGRAPH_VERSION=3.14.3
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
