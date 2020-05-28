fc2cb155725fe0e858a4bddd08019cbd081405e0
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-05-28 00:00:52 -0700

    Update latest release to 3.16.1

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.16.0
+export SOURCEGRAPH_VERSION=3.16.1
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
