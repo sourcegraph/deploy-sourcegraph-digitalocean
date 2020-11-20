27559f347aaf06689990b5e4dd522f90900b77a7
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2020-11-20 12:24:15 +0100

    release: sourcegraph@3.22.0

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.21.2
+export SOURCEGRAPH_VERSION=3.22.0
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
