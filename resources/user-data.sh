595479fcf1a696e12d6cfdbd0d4dd4b40e159e9f
uwedeportivo <534011+uwedeportivo@users.noreply.github.com>
2020-12-07 19:19:29 -0800

    release: sourcegraph@3.22.1 (#70)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.22.0
+export SOURCEGRAPH_VERSION=3.22.1
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
