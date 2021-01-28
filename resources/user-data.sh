bebee37ea33047b5485021b3198d51108c18518f
Gonzalo Peci <pecigonzalo@users.noreply.github.com>
2021-01-28 18:20:26 +0100

    release: sourcegraph@3.24.1

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.24.0
+export SOURCEGRAPH_VERSION=3.24.1
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
