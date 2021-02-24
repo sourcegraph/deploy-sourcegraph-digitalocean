f05068c267cecbec9f6c38fe1e522aba658cb211
Dave Try <davetry@gmail.com>
2021-02-24 12:31:07 -0500

    release: sourcegraph@3.25.1

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.25.0
+export SOURCEGRAPH_VERSION=3.25.1
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
