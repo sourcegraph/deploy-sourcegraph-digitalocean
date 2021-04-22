dab4ce7b81cd8e1384ecd16970444507fee75e5e
Geoffrey Gilmore <geoffrey@sourcegraph.com>
2021-04-22 15:28:37 -0700

    release: sourcegraph@3.27.1 (#84)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.27.0
+export SOURCEGRAPH_VERSION=3.27.1
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
