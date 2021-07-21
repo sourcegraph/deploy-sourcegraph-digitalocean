28b47781bf49732b1e98f34f04be5a35731f5ce6
Dax McDonald <31839142+daxmc99@users.noreply.github.com>
2021-07-21 12:38:58 -0700

    release: sourcegraph@3.30.0

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.29.1
+export SOURCEGRAPH_VERSION=3.30.0
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
