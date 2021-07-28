4344acfed615260fc6a3c9bd8ed40c44639e8a2f
davejrt <2067825+davejrt@users.noreply.github.com>
2021-07-28 13:07:47 -0500

    release: sourcegraph@3.30.2 (#95)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.30.1
+export SOURCEGRAPH_VERSION=3.30.2
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
