aafc02e884f61feb46c367c21a2f989098976f07
Kevin Wojkovich <kevin.wojkovich@sourcegraph.com>
2022-02-02 16:09:58 -0600

    release: sourcegraph@3.36.3 (#118)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.36.2
+export SOURCEGRAPH_VERSION=3.36.3
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
