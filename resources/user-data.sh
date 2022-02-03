ab43b2802ce06b019d5660e196c442959b7cfe68
Michael Lin <mlzc@hey.com>
2022-02-03 11:30:18 -0800

    release: sourcegraph@3.35.2

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.36.3
+export SOURCEGRAPH_VERSION=3.35.2
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
