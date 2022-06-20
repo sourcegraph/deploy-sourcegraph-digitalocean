f2bb14d2c8b00a24b245d3568329ff9200c2c2e7
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2022-06-20 21:49:42 +0100

    release: sourcegraph@3.41.0 (#130)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=3.40.2
+export SOURCEGRAPH_VERSION=3.41.0
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
