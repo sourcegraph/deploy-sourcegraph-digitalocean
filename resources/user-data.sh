18b32bdaf30b835a592c57ea296bc5296f8dd28e
Camden Cheek <camden@ccheek.com>
2022-09-26 18:11:06 -0600

    release: sourcegraph@4.0.1

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=4.0.0
+export SOURCEGRAPH_VERSION=4.0.1
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
