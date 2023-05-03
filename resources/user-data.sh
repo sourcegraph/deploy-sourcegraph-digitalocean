95ec2166878de3bd71900887e7dba31ae03dd232
Joe Chen <joe@sourcegraph.com>
2023-05-03 18:00:48 -0400

    release: sourcegraph@5.0.3 (#157)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=5.0.2
+export SOURCEGRAPH_VERSION=5.0.3
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
