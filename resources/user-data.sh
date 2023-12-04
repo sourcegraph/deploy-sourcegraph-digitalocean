5ce652e71377bd25e2501de8dbf9bf5741813b52
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-12-04 18:04:08 +0100

    release: sourcegraph@5.2.4 (#175)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=5.2.3
+export SOURCEGRAPH_VERSION=5.2.4
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
