41d09131afb1b5c25f18131ce2b23c703dd2e128
Jacob Pleiness <jdpleiness@users.noreply.github.com>
2023-12-13 16:52:45 -0500

    release: sourcegraph@5.2.5 (#176)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=5.2.4
+export SOURCEGRAPH_VERSION=5.2.5
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
