0a42b9d365fb4ba14ffc9bec72657694d9041270
Warren Gifford <warrenbruceg@gmail.com>
2024-01-10 17:16:32 -0800

    release: sourcegraph@5.2.6 (#177)

--- resources/user-data.sh
+++ resources/user-data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export SOURCEGRAPH_VERSION=5.2.5
+export SOURCEGRAPH_VERSION=5.2.6
 export USER_HOME=/root
 export SOURCEGRAPH_CONFIG=/etc/sourcegraph
 export SOURCEGRAPH_DATA=/var/opt/sourcegraph
